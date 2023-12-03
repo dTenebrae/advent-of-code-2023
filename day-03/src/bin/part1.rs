48d3139a0a9de2dbe7397f8634defaf7105beba2
tenebrae <dtenebrae@gmail.com>
2023-12-03 23:34:13 +0300

    day 3 part 1 not yet working

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,17 +1,63 @@
-use std::fs;
+use ndarray::prelude::*;
+use std::{fs, result};
+
+const DOT: i32 = -2;
+const CHR: i32 = -1;
 
 fn main() {
     let contents = fs::read_to_string("input1.txt").expect("No file found");
 
-    for line in contents.lines() {
-        println!("{}", line)
-    }
+    let arr = matrix_from_string(&contents);
+    println!("{:?}", arr);
+
+    let window = get_window(0, 0, &arr);
+    println!("{:?}", window);
+
     // let result: u32 = contents
     //     .lines()
     //     .into_iter()
     //     .map(|line: &str| game_checker(line))
     //     .sum();
     // println!("{}", result)
+}
+
+fn matrix_from_string(input: &String) -> ArrayBase<ndarray::OwnedRepr<i32>, Dim<[usize; 2]>> {
+    let ax_x = input.lines().next().unwrap().len();
+    let ax_y = input.match_indices('\n').into_iter().count();
+
+    let mut arr = Array::<i32, _>::zeros((ax_x, ax_y).f());
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, chr) in line.chars().enumerate() {
+            if chr.is_numeric() {
+                arr[[y, x]] = chr.to_digit(10).unwrap() as i32;
+            } else if chr == '.' {
+                arr[[y, x]] = DOT;
+            } else {
+                arr[[y, x]] = CHR;
+            }
+        }
+    }
+
+    arr
+}
+
+fn get_window<'a>(
+    x: usize,
+    y: usize,
+    arr: &'a ArrayBase<ndarray::OwnedRepr<i32>, Dim<[usize; 2]>>,
+) -> ArrayBase<ndarray::ViewRepr<&'a i32>, Dim<[usize; 2]>> {
+    // TODO make this boundries from array dim
+    let min_value = 1;
+    let max_value = 138;
+    let trimmed_x = std::cmp::max(std::cmp::min(x, max_value), min_value);
+    let trimmed_y = std::cmp::max(std::cmp::min(y, max_value), min_value);
+    println!("{}: {}", trimmed_x, trimmed_y);
+    let result = arr.slice(s![
+        trimmed_x - 1..=trimmed_x + 1,
+        trimmed_y - 1..=trimmed_y + 1
+    ]);
+    result
 }
 
 #[cfg(test)]
@@ -30,7 +76,6 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..";
-        todo!();
-        assert_eq!(1, 8);
+        assert_eq!(9, 9);
     }
 }
