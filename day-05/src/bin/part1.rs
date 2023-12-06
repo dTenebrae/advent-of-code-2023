874fe3eb9bfbf6da93dd041c5c78d4647fb07356
tenebrae <dtenebrae@gmail.com>
2023-12-07 00:24:56 +0300

    day 5 part 1 working

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -1,29 +1,33 @@
 use std::{fs, str::FromStr};
 
 struct RangeMap {
-    src_begin: u64,
-    dest_begin: u64,
-    range: u64,
+    src_begin: i64,
+    dest_begin: i64,
+    range: i64,
 }
 
 trait Contains {
-    fn contains(&self, num: &u64) -> bool;
-    fn dest_num(&self, num: &u64) -> u64;
+    fn contains(&self, num: &i64) -> bool;
+    fn dest_num(&self, num: &i64) -> i64;
 }
 
 impl Contains for RangeMap {
-    fn contains(&self, num: &u64) -> bool {
+    fn contains(&self, num: &i64) -> bool {
         self.src_begin <= *num && *num < (self.src_begin + self.range)
     }
 
-    fn dest_num(&self, num: &u64) -> u64 {
-        self.dest_begin + *num
+    fn dest_num(&self, num: &i64) -> i64 {
+        (self.dest_begin - self.src_begin)  + *num
     }
 }
 
 fn main() {
     let contents = fs::read_to_string("input1.txt").expect("No file found");
 
+    println!("{}", map_seeds(contents));
+}
+
+fn map_seeds(contents: String) -> i64 {
     let mut content_iter = contents.lines();
     let seeds = content_iter.next().unwrap();
     let mut seed_vec = read_seeds(seeds);
@@ -43,6 +47,7 @@ fn main() {
                     for rng in &range_vec {
                         if rng.contains(num) {
                             *num = rng.dest_num(num);
+                            break;
                         }
                     }
 
@@ -52,7 +57,7 @@ fn main() {
         }
 
         if read_flag {
-            let tmp_vec: Vec<u64> = read_values(line).unwrap();
+            let tmp_vec: Vec<i64> = read_values(line).unwrap();
             range_vec.push(RangeMap {
                 src_begin: tmp_vec[1],
                 dest_begin: tmp_vec[0],
@@ -61,15 +66,16 @@ fn main() {
 
         }
     }
-    println!("{}", seed_vec.iter().min().unwrap())
+
+    *seed_vec.iter().min().unwrap()
 }
 
-fn read_seeds(line: &str) -> Vec<u64> {
+fn read_seeds(line: &str) -> Vec<i64> {
     let mut seeds_iter = line.split(":");
     let _ = seeds_iter.next().unwrap();
 
     let seeds = seeds_iter.next().unwrap();
-    read_values::<u64>(seeds).unwrap()
+    read_values::<i64>(seeds).unwrap()
 }
 
 fn read_values<T: FromStr>(line: &str) -> Result<Vec<T>, T::Err> {
@@ -79,12 +85,10 @@ fn read_values<T: FromStr>(line: &str) -> Result<Vec<T>, T::Err> {
         .collect()
 }
 
-fn map_seed2location() -> u64 {
-    todo!();
-}
 
 #[cfg(test)]
 mod test {
+
     use super::*;
 
     #[test]
@@ -122,7 +126,7 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4";
-        let result = map_seed2location();
+        let result = map_seeds(input.to_owned());
         assert_eq!(result, 35);
     }
 }
